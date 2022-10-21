212150da41523c90220b046fc2a156be22821a0f
Paul <pholladay@test.com>
2022-10-20 23:45:47 -0500

    end of class

--- keys.py
+++ keys.py
@@ -1,11 +1,11 @@
 consumer_key = 'Mpq99uFFzn8fyj9lPVQZb7rEs'
 consumer_secret = 'DpsbC4t76HVGXmnW8Tlh0ypYTOSS9ValCWcKAtNYQ4g5vXGLGv'
-access_token = '2190430531-HX94SgcxHIJtNKDnEcKTGihkOueACS8ZAQUA1yh'
-access_token_secret = 'cPya7vG3lw3W3LTPG87m9VYro9T42mblkyXM8Y4buF6WW'
+access_token = '1583151042245664772-SinE3GfSdQ8H4ySxzHnqdjVbo7nLdq'
+access_token_secret = 'xK9GLQ12nmX6trgPmWZVEyW9l31FF4uMFqkZuO5yy9sYo'
 
 mapquest_key = 'YourAPIKey'
 
 
-api_key =  'ERLlmRmvt26YebhbmafevvUlB'
-api_key_secret = '0rL3N8CIItQF8MBCu5zruJYqGE7Ki9NYPTEawkML9BlS6Y8waB'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMKjCgEAAAAAEWqvx6mE3RoX4%2FMmX9Pmk8fWwuA%3DEApIMyOqcDhcf2iwBoL20glObsuADw0TKoEu43jlWqGlpKagIU'
+api_key =  '4SjXy9JcDOmpuuQUZ4y8DVBIP'
+api_key_secret = '3uYHFvjhq1R8yDsJx3xU1L38XealDq4MJMYrFelRNwKzD2nuc6'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAGWSiQEAAAAA7PmEthtAd1yaZTBMr2NDC%2Beqa3I%3D6OP0ucrCc59j6gjc1jaqNxQjjQsy0UcsP21mGSZldcq5rN94yr'
